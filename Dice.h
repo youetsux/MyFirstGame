28942ca260907c6005c8db1ff7c5bdc1e69c7e2b
y.sato <y.sato@jc-21.jp>
2025-07-22 15:13:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Dice.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Quad.h"
+
+
+
+
+class Dice :
+    public Quad
+{
+public:
+	Dice();
+	~Dice();
+	HRESULT Initialize() override;
+	void Draw(XMMATRIX& worldMatrix) override;
+	void Release();
+};
+
