6816d2f96100644186742674084e9f5bc61718f3
y.sato <y.sato@jc-21.jp>
2025-09-09 15:07:57 +0900

    DepthStencil設定するところまで（完了）

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -10,6 +10,9 @@ namespace Direct3D
 	ID3D11DeviceContext* pContext;	                //デバイスコンテキスト
 	IDXGISwapChain* pSwapChain;		                //スワップチェイン
 	ID3D11RenderTargetView* pRenderTargetView;	    //レンダーターゲットビュー
+    ID3D11Texture2D* pDepthStencil;			//深度ステンシル
+    ID3D11DepthStencilView* pDepthStencilView;		//深度ステンシルビュー
+
 
 	struct SHADER_BUNDLE
 	{
@@ -249,10 +252,29 @@ HRESULT Direct3D::Initialize(int winW, int winH, HWND hWnd)
     vp.MaxDepth = 1.0f;	//奥
     vp.TopLeftX = 0;	//左
     vp.TopLeftY = 0;	//上
+	///////////////////////////深度ステンシルビュー作成///////////////////////////////
+        //深度ステンシルビューの作成
+    D3D11_TEXTURE2D_DESC descDepth;
+    descDepth.Width = winW;
+    descDepth.Height = winH;
+    descDepth.MipLevels = 1;
+    descDepth.ArraySize = 1;
+    descDepth.Format = DXGI_FORMAT_D32_FLOAT;
+    descDepth.SampleDesc.Count = 1;
+    descDepth.SampleDesc.Quality = 0;
+    descDepth.Usage = D3D11_USAGE_DEFAULT;
+    descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    descDepth.CPUAccessFlags = 0;
+    descDepth.MiscFlags = 0;
+    pDevice->CreateTexture2D(&descDepth, NULL, &pDepthStencil);
+    pDevice->CreateDepthStencilView(pDepthStencil, NULL, &pDepthStencilView);
+
+
+
 
     //データを画面に描画するための一通りの設定（パイプライン）
     pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);  // データの入力種類を指定
-    pContext->OMSetRenderTargets(1, &pRenderTargetView, nullptr);            // 描画先を設定
+    pContext->OMSetRenderTargets(1, &pRenderTargetView, pDepthStencilView);            // 描画先を設定
     pContext->RSSetViewports(1, &vp);
 
     HRESULT hr;
@@ -271,6 +293,7 @@ void Direct3D::BeginDraw()
     float clearColor[4] = { 0.0f, 0.5f, 0.5f, 1.0f };//R,G,B,A
     //画面をクリア
     pContext->ClearRenderTargetView(pRenderTargetView, clearColor);
+	pContext->ClearDepthStencilView(pDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
 }
 
 void Direct3D::EndDraw()
